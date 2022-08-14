46db2ea7cead356135bd4be4dd439b258348c859
sontrinh16 <trinhleson2000@gmail.com>
2022-08-15 02:16:35 +0700

    fix request handler return json

--- server/serve/controller/pixelController.go
+++ server/serve/controller/pixelController.go
@@ -1,10 +1,7 @@
 package controller
 
 import (
-	"bytes"
-	"encoding/json"
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/notional-labs/pixel/global"
@@ -15,28 +12,10 @@ import (
 )
 
 func GetPixelHandler(c *gin.Context) {
-	// print board
-	var buffer bytes.Buffer
-	var b []byte
-	var err error
 	board := global.GetBoard()
 
-	for _, item := range board {
-		b, err = json.Marshal(item)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": err.Error(),
-			})
-		}
-
-		buffer.WriteString(string(b) + ",")
-	}
-
-	s := strings.TrimSpace(buffer.String())
-	// trim last comma
-
 	c.JSON(http.StatusOK, gin.H{
-		"data": s,
+		"data": board,
 	})
 }
 
